6441269400519d623bb16098d0be36f4aa816adb
vilra830 <villafuerte.rhea@gmail.com>
2025-05-05 14:22:18 +1000

    adds EventRepository to fetch data from database

---
+++ events-calendar/src/main/java/com/vilra830/events_calendar/event/EventRepository.java
@@ -0,0 +1,15 @@
+package com.vilra830.events_calendar.event;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long>{
+    Optional<Event> findEventByDate(LocalDate dateTime);
+    List <Event> findEventsByLabel(String label);
+    List<Event> findEventByLocation(String location);
+    
+
+}
